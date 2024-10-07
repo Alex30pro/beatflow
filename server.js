641e1515fc2f34819f0efb7f59144a90162cca91
Alex30pro <alexandryopg87@gmail.com>
2024-10-07 17:15:30 -0300

    "atualizado"

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const app = require("./src/app");
 const mongoose = require("mongoose");
 
 mongoose
+//`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.CLUSTER_ADDRESS}/${process.env.DB_NAME}`
     .connect("mongodb+srv://alexandryopg87:kUPpFyf3YGEoFBQy@cluster0.7lszy.mongodb.net/")
     .then(() => console.log("Conectado ao MongoDB"))
     .catch((erro) => console.error("Erro ao conectar ao MongoDB:", erro));
